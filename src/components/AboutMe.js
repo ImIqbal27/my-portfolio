7409280c9476b2e55272967f42eb91ab3315026a
Iqbal <iamhasan9501@gmail.com>
2023-03-17 02:33:31 +0600

    init

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const AboutMe = () => {
-
-
-
-    return (
-        <div className='bg-teal-100 flex justify-center'>
-            <div className='w-96  ' >
-                <img src="https://i.ibb.co/3zYngX6/about-me2.png" alt="" />
-                <p>
-                    Myself , <strong> Md Iqbal Hasan Rumon</strong> , graduated from PCIU in CSE.
-                    I want to ba a good web developer .I want to strong my skills with PH.
-                    I am dedicated to my goal.I  enjoy to learning new things about anythings.
-                </p>
-                <p>Email:  iamhasan9501@gmail.com </p>
-                <p>Github: https://github.com/ImIqbal27 </p>
-                <p>Linkedin: https://www.linkedin.com/in/myselfiqbal/</p>
-                <p>Call: +880 1834093013</p>
-                <p>Website: <a href={'https://iqbal-hasan-portfolio.web.app/'} >https://iqbal-hasan-portfolio.web.app </a></p>
-
-              
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="bg-teal-100 flex justify-center py-[162px]">
+      <div className="w-96  ">
+        {/* <img src="https://i.ibb.co/3zYngX6/about-me2.png" alt="" /> */}
+        <p>
+          Myself , <strong> Md Iqbal Hasan Rumon</strong> , graduated from PCIU
+          in CSE. I want to ba a good web developer .I want to strong my skills
+          with PH. I am dedicated to my goal.I enjoy to learning new things
+          about anythings.
+        </p>
+        <p>Email: iamhasan9501@gmail.com </p>
+        <p>Github: https://github.com/ImIqbal27 </p>
+        <p>Linkedin: https://www.linkedin.com/in/myselfiqbal/</p>
+        <p>Call: +880 1834093013</p>
+        <p>
+          Website:{" "}
+          <a href={"https://iqbal-hasan-portfolio.web.app/"}>
+            https://iqbal-hasan-portfolio.web.app{" "}
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default AboutMe;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,42 +1,70 @@
-import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-    return (
-        <div>
-            <div className='bg-teal-300 px-10 py-14'>
-                <div className='text-center pb-14 text-white'>
+  const form = useRef();
 
-                    <h1 className='text-4xl'>Send me a message !</h1>
-                </div>
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-                <div className='grid grid-cols-1 justify-items-center gap-5 '>
-                    <input
-                        type='text'
-                        placeholder='Email Address'
-                        className='input w-full max-w-md'
-                    />
-                    <input
-                        type='text'
-                        placeholder='Subject'
-                        className='input w-full max-w-md'
-                    />
-                    <textarea
-                        className='textarea w-full max-w-md'
-                        placeholder='Your message'
-                        rows={6}
-                    ></textarea>
+    emailjs
+      .sendForm(
+        "service_6w2zsmq",
+        "template_3x28tuc",
+        form.current,
+        "NZn-yAwrllOqT6PQS"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
 
-                    <button type="submit" className="btn btn-accent-900 w-96"> <FontAwesomeIcon icon={faPaperPlane} className='pr-3'></FontAwesomeIcon> Send</button>
-                </div>
-
-
-
-
-            </div>
+    console.log(form.current);
+  };
+  return (
+    <div>
+      <div className="bg-teal-300 px-10 py-14">
+        <div className="text-center pb-14 text-white">
+          <h1 className="text-4xl">Send me a message !</h1>
         </div>
-    );
+
+        <form ref={form} onSubmit={sendEmail}>
+          <div className="grid grid-cols-1 justify-items-center gap-5 ">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              className="input w-full max-w-md"
+            />
+            <input
+              type="text"
+              name="subject"
+              placeholder="Subject"
+              className="input w-full max-w-md"
+            />
+            <textarea
+              name="message"
+              className="textarea w-full max-w-md"
+              placeholder="Your message"
+              rows={6}></textarea>
+
+            <button type="submit" className="btn btn-accent-900  w-[445px]">
+              <FontAwesomeIcon
+                icon={faPaperPlane}
+                className="pr-3"></FontAwesomeIcon>{" "}
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
